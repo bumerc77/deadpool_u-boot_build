07210f29472e4ec0e42ddc4d79104a801f01b93b
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-09 14:32:17 -0400

    build*: Track 13 tag

--- build_m5.sh
+++ build_m5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build.sh android-tv-10.0.0_r1 g12a_odroidc4_v1
+./build.sh android-tv-13.0.0_r1 g12a_odroidc4_v1
 
 ./generate-bins-new.sh fip-collect-g12a-odroidc4-odroidg12-v2015.01-20210623-153349 out/u-boot/build/u-boot.bin
 
--- build_radxa0.sh
+++ build_radxa0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build.sh android-tv-10.0.0_r1 g12a_radxa0_v1
+./build.sh android-tv-13.0.0_r1 g12a_radxa0_v1
 
 ./generate-bins-new.sh fip-radxa0-20210802 out/u-boot/build/u-boot.bin
 
