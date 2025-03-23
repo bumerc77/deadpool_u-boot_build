32c1cff8c25d744d126d45827bada5a649cc1b4c
bumerc77 <bumerc.com@gmail.com>
2025-03-23 19:38:57 +0100

    sm1_bananapim5_v1: Add new FIP-blobs.
    Adding FIP-blobs for M5 new board revision.

--- build_m5-new-rev.sh
+++ build_m5-new-rev.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if [ "${FORCE_RECOVERY}" == "true" ]
-then
-./build.sh android-tv-13.0.0_r1-recovery-only g12a_bananapim5_v1
-elif [ "${CONSOLE_ENABLED}" == "true" ]
-then
-./build.sh android-tv-13.0.0_r1-console g12a_bananapim5_v1
-else
-./build.sh android-tv-13.0.0_r1 g12a_bananapim5_v1
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 [fip-collect dir]"
+	exit
 fi
 
-./generate-bins-new.sh fip-collect-g12a-odroidc4-odroidg12-v2015.01-20210623-153349 out/u-boot/build/u-boot.bin
+if [ "${FORCE_RECOVERY}" == "true" ]
+then
+./build.sh android-tv-13.0.0_r1-recovery-only sm1_bananapim5_v1
+elif [ "${CONSOLE_ENABLED}" == "true" ]
+then
+./build.sh android-tv-13.0.0_r1-console sm1_bananapim5_v1
+else
+./build.sh android-tv-13.0.0_r1 sm1_bananapim5_v1
+fi
+
+./generate-bins-new.sh ${1} out/u-boot/build/u-boot.bin
 
 rm -rf out
--- collect-m5-new-rev_binaries-git-refboard.sh
+++ collect-m5-new-rev_binaries-git-refboard.sh
@@ -19,61 +19,78 @@ fi
 GITBRANCH=${1}
 SOCFAMILY=${2}
 REFBOARD=${3}
-if [[ "$SOCFAMILY" == "sm1" ]]
-then
-    SOCFAMILY="g12a"
-fi
 if ! [[ "$SOCFAMILY" == "g12a" || "$SOCFAMILY" == "g12b" || "$SOCFAMILY" == "sm1" ]]
 then
     echo "${SOCFAMILY} is not supported - should be [g12a, g12b, sm1]"
     usage
     exit 22
 fi
-BIN_LIST="$SOCFAMILY/bl2.bin \
-	  $SOCFAMILY/bl30.bin \
-	  $SOCFAMILY/bl31.bin \
-	  $SOCFAMILY/bl31.img \
-	  $SOCFAMILY/aml_encrypt_$SOCFAMILY "
-FW_LIST="$SOCFAMILY/*.fw"
+if [[ "$SOCFAMILY" == "sm1" ]]
+then
+    SOCFAMILY="g12a"
+fi
+# blob dirs
+bl2="bootloader/uboot-repo/bl2/bin"
+bl30="bootloader/uboot-repo/bl30/bin"
+bl31="bootloader/uboot-repo/bl31_1.3/bin"
+fw="bootloader/uboot-repo/fip"
+ddr="bootloader/uboot-repo/fip/tools/ddr_parse"
+# blx/tools/fw
+BIN_LIST="$bl2/$SOCFAMILY/bl2.bin \
+	  $bl30/$SOCFAMILY/bl30.bin \
+	  $bl31/$SOCFAMILY/bl31.bin \
+	  $bl31/$SOCFAMILY/bl31.img \
+	  $fw/$SOCFAMILY/aml_encrypt_$SOCFAMILY \
+          $fw/$SOCFAMILY/*.fw"
 # path to clone the openlinux repos
 TMP_GIT=$(mktemp -d)
 TMP="fip-collect-${SOCFAMILY}-${REFBOARD}-${GITBRANCH}-$(date +%Y%m%d-%H%M%S)"
 mkdir $TMP
+# get FIP-blobs/ddr_parse
+get_src () {
+    local GITBRANCH="master"
+    git clone -n --depth=1 --filter=tree:0 https://github.com/BPI-SINOVOIP/BPI-S905X3-Android9.git -b $GITBRANCH bpi-src
+    cd bpi-src
+    git sparse-checkout set --no-cone /$bl2/$SOCFAMILY /$bl30/$SOCFAMILY /$bl31/$SOCFAMILY /$fw/$SOCFAMILY /$ddr
+    git checkout
+}
+get_src "$@"
+cd ..
+trap 'rm -rf bpi-src *.bin' EXIT
 # U-Boot
-git clone --depth=2 https://github.com/Dangku/amlogic-u-boot.git -b $GITBRANCH $TMP_GIT/u-boot
+git clone --depth=2 https://github.com/bumerc77/deadpool_u-boot.git -b $GITBRANCH $TMP_GIT/u-boot
+mkdir -p $TMP_GIT/u-boot/fip/tools
 mkdir $TMP_GIT/gcc-linaro-aarch64-none-elf
 wget -qO- https://releases.linaro.org/archive/13.11/components/toolchain/binaries/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux.tar.xz | tar -xJ --strip-components=1 -C $TMP_GIT/gcc-linaro-aarch64-none-elf
 mkdir $TMP_GIT/gcc-linaro-arm-none-eabi
 wget -qO- https://releases.linaro.org/archive/13.11/components/toolchain/binaries/gcc-linaro-arm-none-eabi-4.8-2013.11_linux.tar.xz | tar -xJ --strip-components=1 -C $TMP_GIT/gcc-linaro-arm-none-eabi
 sed -i "s,/opt/gcc-.*/bin/,," $TMP_GIT/u-boot/Makefile
+cp -r bpi-src/$ddr $TMP_GIT/u-boot/fip/tools/
 (
     cd $TMP_GIT/u-boot
     make ${REFBOARD}_defconfig
     PATH=$TMP_GIT/gcc-linaro-aarch64-none-elf/bin:$TMP_GIT/gcc-linaro-arm-none-eabi/bin:$PATH CROSS_COMPILE=aarch64-none-elf- make -j8 > /dev/null
-#    cd fip/tools/ddr_parse && make clean && make
+    cd fip/tools/ddr_parse && make clean && make
+    ./parse ../../../build/board/amlogic/*/firmware/acs.bin
 )
-cp $TMP_GIT/u-boot/build/board/hardkernel/*/firmware/acs.bin $TMP/
+cp $TMP_GIT/u-boot/build/board/amlogic/*/firmware/acs.bin $TMP/
 cp $TMP_GIT/u-boot/build/scp_task/bl301.bin $TMP/
-# cp $TMP_GIT/u-boot/fip/tools/ddr_parse/parse $TMP/
-#$TMP_GIT/u-boot/fip/tools/ddr_parse/parse ${TMP}/acs.bin
+#cp $TMP_GIT/u-boot/fip/tools/ddr_parse/parse $TMP
+#$TMP_GIT/u-boot/fip/tools/ddr_parse/parse $TMP/acs.bin
 # FIP/BLX
 echo $BIN_LIST
 for item in $BIN_LIST
 do
     BIN=$(echo $item)
-    DIR=$TMP_GIT/u-boot/fip/
-
-    if [[ -d $DIR/$SOCFAMILY/ ]]
-    then
-      cp $DIR/$BIN ${TMP}
-    fi
+    cp bpi-src/$BIN ${TMP}
 done
-echo $FW_LIST
-cp $TMP_GIT/u-boot/fip/$FW_LIST ${TMP}
-
 # Normalize
-mv $TMP_GIT/u-boot/fip/$SOCFAMILY/aml_encrypt_$SOCFAMILY $TMP/aml_encrypt
+mv bpi-src/$fw/$SOCFAMILY/aml_encrypt_$SOCFAMILY $TMP/aml_encrypt
 date > $TMP/info.txt
+if [[ "$REFBOARD" == "sm1_bananapim5_v1" ]]
+then
+    SOCFAMILY="sm1"
+fi
 echo "SOC: $SOCFAMILY" >> $TMP/info.txt
 echo "BRANCH: $GITBRANCH ($(date +%Y%m%d))" >> $TMP/info.txt
 for component in $TMP_GIT/*
@@ -85,7 +102,12 @@ do
 done
 
 # use bl30.bin from Khadas as the BPI ne is having a undefined instruction crash
-wget -O $TMP/bl30.bin https://github.com/khadas/android_u-boot/blob/a476c78191f7642aa387ddbdb7296a0158028692/bl30/bin/g12a/bl30.bin?raw=true
+# wget -O $TMP/bl30.bin https://github.com/khadas/android_u-boot/blob/a476c78191f7642aa387ddbdb7296a0158028692/bl30/bin/g12a/bl30.bin?raw=true
 echo "BOARD: $REFBOARD" >> $TMP/info.txt
 echo "export SOCFAMILY=$SOCFAMILY" > $TMP/soc-var.sh
+# extract ddr fw-info
+dd if=$TMP/aml_ddr.fw of=fw_version.bin bs=$((0x1)) count=$((0x13)) skip=$((0xb225))
+dd if=$TMP/aml_ddr.fw of=fw_built.bin bs=$((0x1)) count=$((0x46)) skip=$((0xad78))
+echo "DDR-FIRMWARE: $(< "fw_version.bin")" >> $TMP/info.txt
+echo "$(< "fw_built.bin")" >> $TMP/info.txt
 rm -rf ${TMP_GIT}
