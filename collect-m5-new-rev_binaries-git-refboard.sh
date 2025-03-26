cbfd34b0fcb845fa93bd0592348ef466b57949da
bumerc77 <bumerc.com@gmail.com>
2025-03-26 22:08:29 +0100

    m5-new-rev: Use tmpdir
    * Clone FIP to tmpdir
    * Minor reformatting of the info file

--- collect-m5-new-rev_binaries-git-refboard.sh
+++ collect-m5-new-rev_binaries-git-refboard.sh
@@ -49,14 +49,14 @@ mkdir $TMP
 # get FIP-blobs/ddr_parse
 get_src () {
     local GITBRANCH="master"
-    git clone -n --depth=1 --filter=tree:0 https://github.com/BPI-SINOVOIP/BPI-S905X3-Android9.git -b $GITBRANCH bpi-src
-    cd bpi-src
+    git clone -n --depth=1 --filter=tree:0 https://github.com/BPI-SINOVOIP/BPI-S905X3-Android9.git -b $GITBRANCH $TMP_GIT/FIP
+    (
+    cd $TMP_GIT/FIP
     git sparse-checkout set --no-cone /$bl2/$SOCFAMILY /$bl30/$SOCFAMILY /$bl31/$SOCFAMILY /$fw/$SOCFAMILY /$ddr
     git checkout
+    )
 }
 get_src "$@"
-cd ..
-trap 'rm -rf bpi-src *.bin' EXIT
 # U-Boot
 git clone --depth=2 https://github.com/bumerc77/deadpool_u-boot.git -b $GITBRANCH $TMP_GIT/u-boot
 mkdir -p $TMP_GIT/u-boot/fip/tools
@@ -65,7 +65,7 @@ wget -qO- https://releases.linaro.org/archive/13.11/components/toolchain/binarie
 mkdir $TMP_GIT/gcc-linaro-arm-none-eabi
 wget -qO- https://releases.linaro.org/archive/13.11/components/toolchain/binaries/gcc-linaro-arm-none-eabi-4.8-2013.11_linux.tar.xz | tar -xJ --strip-components=1 -C $TMP_GIT/gcc-linaro-arm-none-eabi
 sed -i "s,/opt/gcc-.*/bin/,," $TMP_GIT/u-boot/Makefile
-cp -r bpi-src/$ddr $TMP_GIT/u-boot/fip/tools/
+cp -r $TMP_GIT/FIP/$ddr $TMP_GIT/u-boot/fip/tools/
 (
     cd $TMP_GIT/u-boot
     make ${REFBOARD}_defconfig
@@ -82,32 +82,33 @@ echo $BIN_LIST
 for item in $BIN_LIST
 do
     BIN=$(echo $item)
-    cp bpi-src/$BIN ${TMP}
+    cp $TMP_GIT/FIP/$BIN ${TMP}
 done
 # Normalize
-mv bpi-src/$fw/$SOCFAMILY/aml_encrypt_$SOCFAMILY $TMP/aml_encrypt
+mv $TMP_GIT/FIP/$fw/$SOCFAMILY/aml_encrypt_$SOCFAMILY $TMP/aml_encrypt
 date > $TMP/info.txt
-if [[ "$REFBOARD" == "sm1_bananapim5_v1" ]]
-then
-    SOCFAMILY="sm1"
-fi
-echo "SOC: $SOCFAMILY" >> $TMP/info.txt
 echo "BRANCH: $GITBRANCH ($(date +%Y%m%d))" >> $TMP/info.txt
 for component in $TMP_GIT/*
 do
     if [[ -d $component/.git ]]
     then
-        echo "$(basename $component): $(git --git-dir=$component/.git log --pretty=format:%H HEAD~1..HEAD)" >> $TMP/info.txt
+        echo "$(basename $component): $(git --git-dir=$component/.git log --pretty=format:%H -1 HEAD)" >> $TMP/info.txt
     fi
 done
 
 # use bl30.bin from Khadas as the BPI ne is having a undefined instruction crash
 # wget -O $TMP/bl30.bin https://github.com/khadas/android_u-boot/blob/a476c78191f7642aa387ddbdb7296a0158028692/bl30/bin/g12a/bl30.bin?raw=true
+# extract ddr fw-info
+dd if=$TMP_GIT/FIP/$fw/$SOCFAMILY/aml_ddr.fw of=$TMP_GIT/fw_version.bin bs=$((0x1)) count=$((0x13)) skip=$((0xb225))
+dd if=$TMP_GIT/FIP/$fw/$SOCFAMILY/aml_ddr.fw of=$TMP_GIT/fw_built.bin bs=$((0x1)) count=$((0x46)) skip=$((0xad78))
+sed -i 's/ :/:/' $TMP_GIT/fw_built.bin | echo "DDR-FIRMWARE: $(< "$TMP_GIT/fw_version.bin")" >> $TMP/info.txt
+echo "$(< "$TMP_GIT/fw_built.bin")" >> $TMP/info.txt
+
+if [[ "$REFBOARD" == "sm1_bananapim5_v1" ]]
+then
+    SOCFAMILY="sm1"
+fi
+echo "SOC: $SOCFAMILY" >> $TMP/info.txt
 echo "BOARD: $REFBOARD" >> $TMP/info.txt
 echo "export SOCFAMILY=$SOCFAMILY" > $TMP/soc-var.sh
-# extract ddr fw-info
-dd if=$TMP/aml_ddr.fw of=fw_version.bin bs=$((0x1)) count=$((0x13)) skip=$((0xb225))
-dd if=$TMP/aml_ddr.fw of=fw_built.bin bs=$((0x1)) count=$((0x46)) skip=$((0xad78))
-echo "DDR-FIRMWARE: $(< "fw_version.bin")" >> $TMP/info.txt
-echo "$(< "fw_built.bin")" >> $TMP/info.txt
 rm -rf ${TMP_GIT}
